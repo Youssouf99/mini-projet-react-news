cb13269f495c7dab9ec537d38db1c560c6655cc8
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-04-23 16:42:13 +0200

    feat(ConfigBackend): add proxy

--- NewsApi/src/main/java/com/news/site/newsapi/config/MapStructConfig.java
+++
@@ -1,7 +0,0 @@
-package com.news.site.newsapi.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-public class MapStructConfig {
-}
