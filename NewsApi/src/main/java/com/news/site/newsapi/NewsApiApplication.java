7b8cba32bfcd54a93a6bf6bf17fd111786f6ac2e
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-04-23 15:57:14 +0200

    feat : add la config api externe

--- NewsApi/src/main/java/com/news/site/newsapi/NewsApiApplication.java
+++ NewsApi/src/main/java/com/news/site/newsapi/NewsApiApplication.java
@@ -1,7 +1,9 @@
 package com.news.site.newsapi;
 
+import com.news.site.newsapi.config.WebClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class NewsApiApplication {
