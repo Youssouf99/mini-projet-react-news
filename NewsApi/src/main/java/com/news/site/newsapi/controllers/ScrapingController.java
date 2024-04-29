9dd3efe05592091901f2594f06c3ff9e219fe2da
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-04-29 16:30:45 +0200

    feat(Controller): add Controller newsBoulogne

--- NewsApi/src/main/java/com/news/site/newsapi/controllers/ScrapingController.java
+++ NewsApi/src/main/java/com/news/site/newsapi/controllers/ScrapingController.java
@@ -1,2 +1,29 @@
-package com.news.site.newsapi.controllers;public class ScrapingController {
+package com.news.site.newsapi.controllers;
+
+import com.news.site.newsapi.dtos.ArticleDTO;
+import com.news.site.newsapi.services.ScrapingService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/scraping/news")
+@AllArgsConstructor
+public class ScrapingController {
+
+    public ScrapingService scrapingService;
+
+
+    @GetMapping()
+    public List<ArticleDTO> getArticles(
+            @RequestParam(name = "page",
+                    defaultValue = "1") int pageNumber) throws IOException {
+        return scrapingService.fetchArticles(pageNumber);
+    }
+
 }
