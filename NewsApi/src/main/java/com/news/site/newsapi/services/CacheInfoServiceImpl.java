77787d6cfa35d2f912c7b3ce928aec2dfbf98ab8
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-04-29 16:35:02 +0200

    feat(Servies): add service cahce

--- NewsApi/src/main/java/com/news/site/newsapi/services/CacheInfoServiceImpl.java
+++ NewsApi/src/main/java/com/news/site/newsapi/services/CacheInfoServiceImpl.java
@@ -1,2 +1,32 @@
-package com.news.site.newsapi.services;public class CacheInfoServiceImpl {
+package com.news.site.newsapi.services;
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class CacheInfoServiceImpl implements CacheInfoService {
+    private final CacheManager cacheManager;
+    public CacheInfoServiceImpl(CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
+
+    @Override
+    public Map<String, Object> getCacheData(String cacheName) {
+        Map<String, Object> cacheData = new HashMap<>();
+        Cache cache = cacheManager.getCache(cacheName);
+        if (cache != null) {
+            cacheData.put("name", cache.getName());
+            cacheData.put("size", cacheManager.getCache(cacheName));
+            cacheData.put("content", cacheManager.getCache(cacheName).getNativeCache());
+        }
+        return cacheData;
+    }
+
+    public void clearCache() {
+        cacheManager.getCache("latestNews").clear(); // Replace "myCacheName" with your actual cache name
+    }
 }
