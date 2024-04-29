b6343302d26bdd1d06d40a125969df6c3aad65ef
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-04-29 16:33:44 +0200

    feat(Mappers): add NewMapper and BeanUtilsMappers

--- NewsApi/src/main/java/com/news/site/newsapi/mappers/BeanUtilsMapper.java
+++ NewsApi/src/main/java/com/news/site/newsapi/mappers/BeanUtilsMapper.java
@@ -1,2 +1,28 @@
-package com.news.site.newsapi.mappers;public class BeanUtilsMapper {
+package com.news.site.newsapi.mappers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.news.site.newsapi.dtos.SourceDTO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class BeanUtilsMapper {
+
+
+    public static SourceDTO jsonNodeToSourceDto(JsonNode source) {
+        SourceDTO sourceDTO = new SourceDTO();
+        try {
+            if (source.has("id")) {
+                sourceDTO.setId(source.get("id").asText());
+            }
+            if (source.has("name")) {
+                sourceDTO.setName(source.get("name").asText());
+            }
+        } catch (Exception e) {
+            // Gérer l'exception si nécessaire
+            e.printStackTrace();
+        }
+        return sourceDTO;
+    }
 }
--- NewsApi/src/main/java/com/news/site/newsapi/mappers/NewsMapper.java
+++ NewsApi/src/main/java/com/news/site/newsapi/mappers/NewsMapper.java
@@ -1,15 +1,17 @@
 package com.news.site.newsapi.mappers;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.news.site.newsapi.dtos.ArticleDTO;
+import com.news.site.newsapi.dtos.SourceDTO;
+import com.news.site.newsapi.dtos.SourceDetailDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
-import org.springframework.stereotype.Component;
+import org.mapstruct.factory.Mappers;
 
-@Component
 @Mapper(componentModel = "spring")
-public interface ArticleMapper {
+public interface NewsMapper {
 
     @Mappings({
             @Mapping(target = "author", expression = "java(articleNode.has(\"author\") ? articleNode.get(\"author\").asText() : null)"),
@@ -20,5 +22,18 @@ public interface ArticleMapper {
             @Mapping(target = "publishedAt", expression = "java(articleNode.has(\"publishedAt\") ? articleNode.get(\"publishedAt\").asText() : null)"),
             @Mapping(target = "content", expression = "java(articleNode.has(\"content\") ? articleNode.get(\"content\").asText() : null)")
     })
-    ArticleDTO jsonNodeToDto(JsonNode articleNode);
+    ArticleDTO jsonNodeToArticleDto(JsonNode articleNode);
+
+    @Mappings({
+            @Mapping(target = "id", expression = "java(sourceNode.has(\"id\") ? sourceNode.get(\"id\").asText() : null)"),
+            @Mapping(target = "name", expression = "java(sourceNode.has(\"name\") ? sourceNode.get(\"name\").asText() : null)"),
+            @Mapping(target = "description", expression = "java(sourceNode.has(\"description\") ? sourceNode.get(\"description\").asText() : null)"),
+            @Mapping(target = "url", expression = "java(sourceNode.has(\"url\") ? sourceNode.get(\"url\").asText() : null)"),
+            @Mapping(target = "category", expression = "java(sourceNode.has(\"category\") ? sourceNode.get(\"category\").asText() : null)"),
+            @Mapping(target = "language", expression = "java(sourceNode.has(\"language\") ? sourceNode.get(\"language\").asText() : null)"),
+            @Mapping(target = "country", expression = "java(sourceNode.has(\"country\") ? sourceNode.get(\"country\").asText() : null)")
+    })
+    SourceDetailDTO jsonNodeToSourceDetailDto(JsonNode sourceNode);
+
+
 }
